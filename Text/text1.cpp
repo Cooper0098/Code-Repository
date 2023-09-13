f1b49d827f12721b8c23a80d90c14952e84c15d7
Cooper0098 <zhanghaomin97@163.com>
2023-09-13 21:15:34 +0800

    提交说明

--- Text/text1.cpp
+++ Text/text1.cpp
@@ -1,79 +1,24 @@
 #include <iostream>
-#include <vector>
 
-using namespace std;
+#include <cmath>
+#include <algorithm>
+#include <chrono>
 
-// 二叉树节点的定义
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
+#include <cstdlib>
+#include <cstdio>
 
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-};
+using namespace std; // 模拟散列表     现在要进行N次操作，对于每个询问操作输出对应的结果。  哈希表
 
-// 先序遍历二叉树，并输出节点值
-void preorderTraversal(TreeNode *root, vector<int> &result)
-{
-    if (root == nullptr)
-    {
-        return;
-    }
-
-    result.push_back(root->val);            // 输出当前节点的值
-    preorderTraversal(root->left, result);  // 递归遍历左子树
-    preorderTraversal(root->right, result); // 递归遍历右子树
-}
-
-// 遍历二叉树，找到所有叶子节点
-void findLeafNodes(TreeNode *root, vector<int> &leafNodes)
-{
-    if (root == nullptr)
-    {
-        return;
-    }
-
-    if (root->left == nullptr && root->right == nullptr)
-    {
-        leafNodes.push_back(root->val); // 当前节点是叶子节点
-        return;
-    }
-
-    findLeafNodes(root->left, leafNodes);  // 递归遍历左子树
-    findLeafNodes(root->right, leafNodes); // 递归遍历右子树
-}
+const int N = 100010;
 
 int main()
 {
-    // 构建二叉树
-    TreeNode *root = new TreeNode(1);
-    root->left = new TreeNode(2);
-    root->right = new TreeNode(3);
-    root->left->left = new TreeNode(4);
-    root->left->right = new TreeNode(5);
-    root->right->left = new TreeNode(6);
-    root->right->right = new TreeNode(7);
+  
+        bool flag = true;
+    
 
-    // 先序遍历并输出结果
-    vector<int> preorderResult;
-    preorderTraversal(root, preorderResult);
-    cout << "先序遍历序列：";
-    for (int val : preorderResult)
-    {
-        cout << val << " ";
-    }
-    cout << endl;
+        if (flag)
 
-    // 查找叶子节点并输出结果
-    vector<int> leafNodes;
-    findLeafNodes(root, leafNodes);
-    cout << "叶子节点：";
-    for (int val : leafNodes)
-    {
-        cout << val << " ";
-    }
-    cout << endl;
-
+        
     return 0;
 }
