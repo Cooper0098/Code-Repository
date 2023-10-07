864f01fd23c213b432925f8d2838fd27cab823a3
Cooper0098 <zhanghaomin97@163.com>
2023-10-07 23:47:47 +0800

    测试5提交

---
+++ Text/text5.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+#include <cmath>
+#include <algorithm>
+#include <chrono>
+
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    
+}
