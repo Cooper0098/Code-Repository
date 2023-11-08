d73f9206ad86bfbef8da252972e1c9085f57e2f1
Cooper0098 <zhanghaomin97@163.com>
2023-11-08 22:31:32 +0800

    双指针算法

--- C++_colletion/Practice1/24-NotRepeatSubsequences.cpp
+++ C++_colletion/Practice1/24-NotRepeatSubsequences.cpp
@@ -5,8 +5,30 @@
 
 using namespace std;
 
+const int N = 1000010;
+int n ;
+int a[N], s[N];
+
 int main()
-{
+{ 
+    
+    cin >> n;
+    for (int i = 0; i < n;i++)
+    {
+        cin >> a[i];
+    }
+    int res = 0;
+    for (int i = 0, j = 0; i < n;i++)
+    {
+        s[a[i]]++;//默认全部为0,这个位置变成1  记录数组a[i]中每个数出现的次数
+        while (s[a[i]] > 1)
+        {
+            s[a[j]]--;
+            j++;
+        }
+        res = max(res, i - j + 1);
+    }
+    cout << res << endl;
 
     return 0;
 }
