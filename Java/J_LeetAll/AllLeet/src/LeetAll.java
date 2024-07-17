e703997264395b2e64dd2ef893eb1f58c42f0ff2
Cooper0098 <zhanghaomin97@163.com>
2024-07-17 09:20:51 +0800

    send11

---
+++ Java/J_LeetAll/AllLeet/src/LeetAll.java
@@ -0,0 +1,31 @@
+public class LeetAll {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+class Solution {
+
+    public int incremovableSubarrayCount(int[] a) {
+        int n = a.length;
+        int i = 0;
+        while (i < n - 1 && a[i] < a[i + 1]) {
+            i++;
+
+        }
+        if (i == n - 1) {
+            return n * (n + 1) / 2;
+
+        }
+        int ans = i + 2;
+
+        for (int j = n - 1; j == n - 1 || a[j] < a[j + 1]; j--) {
+            while (i >= 0 && a[i] >= a[j]) {
+                i--;
+            }
+        }
+        return ans;
+
+    }
+
+}
