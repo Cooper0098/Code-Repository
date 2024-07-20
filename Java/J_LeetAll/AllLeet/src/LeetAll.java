6676ae1b8f3497bab4416318b6919ef354ae6988
Cooper0098 <zhanghaomin97@163.com>
2024-07-20 22:31:36 +0800

    send

--- Java/J_LeetAll/AllLeet/src/LeetAll.java
+++ Java/J_LeetAll/AllLeet/src/LeetAll.java
@@ -69,55 +69,8 @@ class Solution {
 
     }
 
-    int ans = Integer.MAX_VALUE;
-    int[][] grid;
-    public int minimumMoves(int[][] grid) {
-        this.grid = grid;
-        int cnt = 0;
-        for(int i = 0 ; i < 3 ; i++)
-        {
-            for (int j = 0; j < 3; j++) {
-                if (grid[i][j] == 0)
-                    cnt++;
-            }
-        }
-        
-        dfs(0, cnt);
-        return ans;
 
-        
-
-    }
-
-    private void dfs(int sum, int cnt)
-    {
-        if(cnt == 0) 
-            ans = Math.min(sum, ans);
-
-            for(int i = 0 ; i < 3 ; i++)
-            {
-                for(int j = 0 ; j < 3;j++)
-                {
-                    if(grid[i][j] == 0)
-                    {
-                        for(int k = 0 ; k <3;k++)
-                        {
-                            for(int l = 0 ; l <3;l++)
-                            {
-                                if(grid[k][l] > 1)
-                                {
-                                    grid[k][l]--;
-                                    grid[i][j] = 1;
-                                    dfs(sum + Math.abs(i - k) + Math.abs(j - l), cnt - 1);
-                                    grid[k][l]++;
-                                    grid[i][j] = 0;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-    }
+    
 
 }
 
