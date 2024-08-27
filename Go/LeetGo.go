e10e79c21865e0ac51d2a71ba18487ba9c49496a
Cooper0098 <zhanghaomin97@163.com>
2024-08-27 13:10:55 +0800

    0827

--- Go/LeetGo.go
+++ Go/LeetGo.go
@@ -22,6 +22,8 @@ func twoSum(nums []int, target int) []int {
 
 }
 
+
+
 // --------------------------------------Go-------------------------------------//
 //
 //
@@ -37,28 +39,15 @@ func twoSum(nums []int, target int) []int {
 
 func main() {
 
-	fmt.Println("Hello, World!") //////////////////////////////
+	fmt.Println("Hello, World!")
+	///////////////////////////////////////////////////////////////////////
 	// 测试用例
-	nums := []int{2, 7, 11, 15}
-	target := 9
-	result := twoSum(nums, target)
+	// nums := []int{2, 7, 11, 15}
+	// target := 9
+	// result := twoSum(nums, target)
 
-	fmt.Printf("输入数组: %v, 目标值: %d, 结果: %v\n", nums, target, result)
+	// fmt.Printf("输入数组: %v, 目标值: %d, 结果: %v\n", nums, target, result)
 
-	///////
 
-	name := "Alice"
-
-	age := 30
-
-	// 使用 fmt.Println
-	fmt.Println("Name:", name, "Age:", age) //输出：Name: Alice Age: 30
-
-	// 使用 fmt.Printf
-	fmt.Printf("Name: %s, Age: %d\n", name, age) // 输出：Name: Alice, Age: 30
-
-	for i := 0; i < 5; i++ {
-		fmt.Printf("%d\n", i)
-	}
 
 }
