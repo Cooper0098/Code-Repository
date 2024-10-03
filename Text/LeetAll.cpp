d3710e3aacfbd08ce464720b72e4892114c86dd3
Cooper0098 <zhanghaomin97@163.com>
2024-10-03 23:41:40 +0800

    111

--- Text/LeetAll.cpp
+++ Text/LeetAll.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <vector>
-#include <unordered_map> // 使用哈希表
-#include <unordered_set>
-#include <stack>  // 使用栈
-#include <string> // 使用字符串
-#include <utility>
-#include <cstring> // 有memset将一块内存区域设置为特定的值。 strcpy：复制一个字符串。strcat：连接两个字符串。strlen：获取字符串的长度。strcmp：比较两个字符串。
+#include <iostream> // 输入输出 cin/cout
+#include <cstdio>   // 输入输出 printf/scanf
+//------------------------------------------//
+#include <algorithm>     // 算法, 用于操作容器, 可以用于排序、查找、修改和管理数据
+#include <vector>        // 容器
+#include <string>        // 字符串
+#include <stack>         // 栈, 后进先出
+#include <queue>         // 队列, 先进先出
+#include <unordered_map> // 哈希表, 存储的是键值对. 每个元素由一个键和一个与之关联的值组成, 键是唯一的, 但值可以重复. 适合用于需要根据键快速查找值的场景
+#include <unordered_set> // 哈希表,存储的是唯一元素的集合. 每个元素都是值, 不关联任何其他数据. 适合用于需要存储不重复的元素的场景
+//------------------------------------------//
+#include <memory> // 内存管理
 #include <functional>
 #include <numeric>
 #include <ranges>
-#include <memory>
-#include <queue>
+#include <cstring> // 有memset将一块内存区域设置为特定的值。 strcpy：复制一个字符串。strcat：连接两个字符串。strlen：获取字符串的长度。strcmp：比较两个字符串。
 using namespace std;
 // typedef long long ll;       // 定义long long类型的简写为ll
 // typedef long double ld;     // 定义long double类型的简写为ld
@@ -1008,6 +1009,16 @@ public:
 
             i = (i + 1) % tickets.size();
         }
+        return ans;
+    }
+
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+
+        int ans = 0;
+
+        int neg = -1;
+
         return ans;
     }
 
--- Text/Race.cpp
+++ Text/Race.cpp
@@ -18,16 +18,49 @@ using namespace std;
 // {
 // public:
 // };
+int ans = 0; // 最大价值
+const int maxN = 1006;
 
+int f[maxN][maxN]; //
+int volume[maxN];
+int weight[maxN];
 
 int main()
 {
-    
 
+    int n, m;
+    cin >> n >> m;
 
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> volume[i] >> weight[i];
+    }
 
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+        {
+            if (j < volume[i])//  当前背包容量装不进第i个物品，则价值等于前i-1个物品
+            {
+                f[i][j] = f[i - 1][j];
+            }
+            else
+            {
+                   // 能装，需进行决策是否选择第i个物品
+                f[i][j] = max(f[i - 1][j], f[i - 1][j - volume[i]] + weight[i]);// j - 容量 = 能装载的东西 
+            }
+        }
+    ans = f[n][m];
 
+    cout << ans << endl;
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cout << f[i][j] << "  ";
+        }
+        cout << endl;
+    }
 
     return 0;
-    
 }
