f666db93651afcd59d7ed0cc572e006de6be6e8f
Cooper0098 <zhanghaomin97@163.com>
2024-10-09 22:33:41 +0800

    111

--- Text/LeetAll.cpp
+++ Text/LeetAll.cpp
@@ -988,7 +988,38 @@ public:
         return ans;
     }
 
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost)
+    {
 
+        int current = 0;
+        int cntSum = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.size(); i++)
+        {
+            current += gas[i] - cost[i];
+            cntSum += gas[i] - cost[i];
+
+            if (current < 0)
+            {
+                start = (i + 1) % gas.size();
+
+                current = 0;
+            }
+        }
+        if (cntSum < 0)
+        {
+            return -1;
+        }
+        return start;
+    }
+
+
+
+
+
+
+    
 
     // -----------------------------------Cpp-----------------------------------//
     //
