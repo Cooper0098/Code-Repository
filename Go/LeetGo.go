dab48d501415d25c1628636e3a4f59b79a445bc9
Cooper0098 <zhanghaomin97@163.com>
2024-11-14 00:08:55 +0800

    12......8

--- Go/LeetGo.go
+++ Go/LeetGo.go
@@ -245,6 +245,26 @@ func losingPlayer(x int, y int) string {
 
 }
 
+func countKConstraintSubstrings(s string, k int) (ans int) {
+
+	cnt := [2]int{}
+
+	l := 0
+
+	for r, c := range s {
+
+		cnt[c-'0']++
+		for ; cnt[0] > k && cnt[1] > k; l++ {
+
+			cnt[s[l]-'0']--
+		}
+
+		ans += r - l + 1
+	}
+
+	return
+}
+
 
 
 
