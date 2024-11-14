5cc3ff8daa6f438abf73dbd32599e218190dec02
Cooper0098 <zhanghaomin97@163.com>
2024-11-14 16:33:04 +0800

    16........

---
+++ Leetcode/Hot100cpp/Hot100.cpp
@@ -0,0 +1,31 @@
+#include <iostream> // 输入输出 cin/cout
+#include <cstdio>   // 输入输出 printf/scanf
+//------------------------------------------//
+#include <algorithm>     // 算法, 用于操作容器, 可以用于排序、查找、修改和管理数据
+#include <vector>        // 容器
+#include <string>        // 字符串
+#include <stack>         // 栈, 后进先出
+#include <queue>         // 队列, 先进先出
+#include <unordered_map> // 哈希表, 存储的是键值对. 每个元素由一个键和一个与之关联的值组成, 键是唯一的, 但值可以重复. 适合用于需要根据键快速查找值的场景
+#include <unordered_set> // 哈希表,存储的是唯一元素的集合. 每个元素都是值, 不关联任何其他数据. 适合用于需要存储不重复的元素的场景
+//------------------------------------------//
+#include <memory> // 智能指针
+#include <functional>
+#include <numeric>
+#include <ranges>
+#include <cstring> // 有memset将一块内存区域设置为特定的值。 strcpy：复制一个字符串。strcat：连接两个字符串。strlen：获取字符串的长度。strcmp：比较两个字符串。
+#include <bitset>
+#include <cmath>
+using namespace std;
+
+
+//-------------------------------------------
+
+
+
+
+
+int main()
+{
+    cout << "aaaaaaaaa1111" << endl;
+}
---
+++ Leetcode/Hot100go/Hot100.go
@@ -0,0 +1,16 @@
+package main
+
+
+import (
+	"fmt"
+	// "math"
+	// "sort"
+	// "time"
+)
+
+func main() {
+
+
+    fmt.Println("aaaa")
+
+}
