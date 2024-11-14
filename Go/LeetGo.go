ade50e1f1bc6c14ecdca31c7a80bd5b204ca83db
Cooper0098 <zhanghaomin97@163.com>
2024-11-14 15:15:46 +0800

    15.........15

--- Go/LeetGo.go
+++ Go/LeetGo.go
@@ -265,9 +265,46 @@ func countKConstraintSubstrings(s string, k int) (ans int) {
 	return
 }
 
+func countGoodNodes(edges [][]int) int {
+	ans := 0
 
+	n := len(edges) + 1
 
+	g := make([][]int, n)
 
+	for _, e := range edges {
+		x, y := e[0], e[1]
+		g[x] = append(g[x], y)
+		g[y] = append(g[y], x)
+	}
+
+	var dfs func(int, int) int
+
+	dfs = func(x, fa int) int {
+		size, sz0, ok := 1, 0, true
+		for _, y := range g[x] {
+			if y == fa {
+				continue
+			}
+			sz := dfs(y, x)
+			if sz0 == 0 {
+				sz0 = sz
+			} else if sz != sz0 {
+				ok = false
+			}
+
+			size += sz
+		}
+		if ok {
+			ans++
+		}
+
+		return size
+	}
+	dfs(0, -1)
+	return ans
+
+}
 
 // --------------------------------------Go-------------------------------------//
 //
