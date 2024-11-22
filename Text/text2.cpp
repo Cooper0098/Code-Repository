e739665961d89548b5c9d17a2dd1623e41fe9e89
Cooper0098 <zhanghaomin97@163.com>
2024-11-23 00:10:42 +0800

    12.......

--- Text/text2.cpp
+++ Text/text2.cpp
@@ -1,36 +1,31 @@
-#include <iostream> // 输入输出 cin/cout
-#include <cstdio>   // 输入输出 printf/scanf
-//------------------------------------------//
-#include <algorithm>     // 算法, 用于操作容器, 可以用于排序、查找、修改和管理数据
-#include <vector>        // 容器
-#include <string>        // 字符串
-#include <stack>         // 栈, 后进先出
-#include <queue>         // 队列, 先进先出
-#include <unordered_map> // 哈希表, 存储的是键值对. 每个元素由一个键和一个与之关联的值组成, 键是唯一的, 但值可以重复. 适合用于需要根据键快速查找值的场景
-#include <unordered_set> // 哈希表,存储的是唯一元素的集合. 每个元素都是值, 不关联任何其他数据. 适合用于需要存储不重复的元素的场景
-//------------------------------------------//
-#include <memory> // 内存管理
-#include <functional>
-#include <numeric>
-#include <ranges>
-#include <cstring> // 有memset将一块内存区域设置为特定的值。 strcpy：复制一个字符串。strcat：连接两个字符串。strlen：获取字符串的长度。strcmp：比较两个字符串。
-using namespace std;
-typedef long long ll;       // 定义long long类型的简写为ll
-typedef long double ld;     // 定义long double类型的简写为ld
-typedef pair<int, int> pii; // 定义pair<int, int>类型的简写为pii
-typedef pair<ll, ll> pll;   // 定义pair<ll, ll>类型的简写为pll
-typedef vector<int> vi;     // 定义vector<int>类型的简写为vi
-//--------------------------------模板--------------------------------//
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <iostream>      // cin/cout
+#include <cstdio>        // printf/scanf
 
-bool check(int x)
-{
+using namespace std;
+
+typedef long long ll;       // long long为ll
+typedef long double ld;     // long doubleld
+typedef pair<int, int> pii; // pair<int, int>pii
+typedef pair<ll, ll> pll;   // pair<ll, ll>pll
+typedef vector<int> vi;     // vector<int>vi
+
+const long long inf = numeric_limits<long long>::max(); // 无穷大
+const int N = 100100; // 100100 十万 , 1100100 一百万 , 100100100 一亿
+
+void Mysolve(){
+    
 }
 
-int main()
-{
-    int a, b;
-    a = 5, b = 2;
-    cout << a / b;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //---------优化
+
+    Mysolve();
 
     return 0;
 }
+
